bffb51832458deea72cfb3bee31e959442731214
sachinggsingh <152975718+sachinggsingh@users.noreply.github.com>
2025-05-06 19:51:14 +0530

    Added Avatar Functionality

--- Frontend/client/src/components/Navbar.jsx
+++ Frontend/client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, X, Home, Info, Settings, Mail, PawPrint } from 'lucide-react';
+import Avatar from './Avatar';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -40,8 +41,9 @@ const Navbar = () => {
             <NavLink href="#" icon={<Info size={18} />} text="About" />
             <NavLink href="#" icon={<Mail size={18} />} text="Contact" />
             <NavLink href="#" icon={<Settings size={18} />} text="Settings" />
+            <Avatar />
 
-            <div className="flex space-x-4 ml-4">
+            {/* <div className="flex space-x-4 ml-4">
               <a href="/login">
                 <button className="px-4 py-2 text-sm font-medium text-indigo-600 border border-indigo-600 rounded hover:bg-indigo-50 transition-colors duration-200">
                   Login
@@ -52,7 +54,7 @@ const Navbar = () => {
                   Sign Up
                 </button>
               </a>
-            </div>
+            </div> */}
           </div>
 
           {/* Mobile menu button */}
@@ -80,6 +82,9 @@ const Navbar = () => {
           <MobileNavLink href="#" icon={<Info size={18} />} text="About" />
           <MobileNavLink href="#" icon={<Mail size={18} />} text="Contact" />
           <MobileNavLink href="#" icon={<Settings size={18} />} text="Settings" />
+          <div className="mt-4 px-4">
+            <Avatar />
+          </div>
 
           <div className="px-3 space-y-2 mt-2">
             <a href="/login" className="block">
